3e1b2c7fb8b30283efbc3421a0b16d4e5cd8856f
Jason Sugiharto <jasonliesugiharto@gmail.com>
2025-06-28 12:29:53 -0700

    updated the animation for the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -108,56 +108,54 @@ function NavBarScroll () {
 
   const buttonRef = useRef();
   const containerRef = useRef();
-  const linkRef = useRef();
-  linkRef.current = [];
-
-  const addToRefs = (el) => {
-    if(el && !linkRef.current.includes(el)){
-      linkRef.current.push(el);
-    }
-  }
+  const t1 = gsap.timeline();
 
   useGSAP(() => {
-    const buttonT1 = gsap.timeline({defaults: {ease: "power4.out"}})
-    const masterT2 = gsap.timeline({defaults: {ease: "power3.out"}})
-
-
-    buttonT1.fromTo(buttonRef.current, 
-      { scale: 0, opacity: 0 }, 
-      { scale: 1, opacity: 1, duration: 0.6 }
-    )
+    t1.fromTo(buttonRef.current,{
+      opacity: 0,
+      scale: 0
+    }, {
+      opacity: 1,
+      scale: 1,
+      duration: 0.2,
+      ease: "power1.inOut"
+    })
   }, [])
 
   useGSAP(() => {
-    const masterT1 = gsap.timeline({defaults: {ease: "power3.inOut"}})
 
-    if (active) {
-      // Opening animation
-      masterT1.fromTo(containerRef.current, 
-        { x: "100%", opacity: 1},
-        { x: "0%", duration: 0.3, ease: "power3.out"}
-      );
+    if (active){
+      t1.fromTo(containerRef.current, {
+        opacity: 0,
+        x: 300
+      },{
+        opacity: 1,
+        x: 0,
+        duration: 0.2,
+        ease: "power1.inOut"
+      })
 
-      // Stagger animation for links
-      masterT1.fromTo(linkRef.current,
-        { x: 50, opacity: 0 },
-        { x: 0, opacity: 1, stagger: 0.1, duration: 0.7 },
-        "-=0.2" // Slight overlap with previous animation
-      );
+      t1.fromTo("#navbar-item", {
+        x: 100,
+        opacity: 0
+      }, {
+        opacity: 1,
+        x: 0,
+        ease: "power1.inOut",
+        // duration: 0.2
+        stagger: 0.1
+      })
     }
     else{
-      // Closing animation
-      if(containerRef.current){
-        gsap.to(containerRef.current, 
-          {
-            x: "100%",
-            duration: 0.5,
-            ease: "power1.in"
-          }
-        )
-      }
+      t1.to(containerRef.current, {
+        opactiy: 0,
+        x: 700,
+        ease: "power3.in",
+        duration: 0.3,
+      })
     }
-  }, { dependencies: [active] })
+  }, [active])
+
   const links = [
     {
         id: 1,
@@ -196,10 +194,11 @@ function NavBarScroll () {
         <span className={`w-7 h-[2px] bg-secondary absolute rounded-full ${active ? "translate-y-0 rotate-45": "-translate-y-1 rotate-0"} duration-300`}></span>
         <span className={`w-7 h-[2px] bg-secondary absolute rounded-full ${active ? "translate-y-0 -rotate-45": "translate-y-1 rotate-0"} duration-300`}></span>
       </button>
-      {active && 
+
+      {/* {active &&  */}
       <div
       ref={containerRef}
-      style={{ transform: 'translateX(100%)' }}
+      style={{ transform: 'translateX(100%)', opacity: 0 }}
       className='w-full h-screen fixed top-0 left-0 z-30 justify-end duration-300'>
         <div className='w-full h-screen flex justify-end'>
           <div className='w-[36em] lg:max-w-3xl flex flex-col justify-end bg-fourth'>
@@ -207,7 +206,8 @@ function NavBarScroll () {
             <nav className='relative w-full h-full px-10 leading-tight flex flex-col mt-20'>
               {links.map(links => (
                 <li 
-                ref={addToRefs}
+                id='navbar-item'
+                // ref={addToRefs}
                 key={links.id} className='text-[4rem] text-primary font-medium group relative flex w-fit gap-4 cursor-pointer items-center'>
                   <span className='w-3 h-3 absolute invisible opacity-0 bg-primary rounded-full group-hover:visible group-hover:opacity-100 group-hover:scale-100 scale-0 duration-300'></span>
                   <a href={links.link} onClick={() => setActive(false)} className='group-hover:translate-x-7 duration-700 ease-out'>
@@ -218,7 +218,8 @@ function NavBarScroll () {
             </nav>
           </div>
         </div>
-      </div>}
+      </div>
+      {/* } */}
     </>
   )
 }
